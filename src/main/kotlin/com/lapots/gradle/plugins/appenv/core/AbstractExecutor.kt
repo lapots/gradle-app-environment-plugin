c358288effd59d9666060ae5e8f34bb60d162e18
Lapots <sebastrident@gmail.com>
2017-05-02 20:19:05 +0300

    Removed Kotlin sources

--- src/main/kotlin/com/lapots/gradle/plugins/appenv/core/AbstractExecutor.kt
+++
@@ -1,5 +0,0 @@
-package com.lapots.gradle.plugins.appenv.core
-
-abstract class AbstractExecutor(var extension: ApplicationEnvironmentExtension) {
-    abstract fun execute()
-}
--- src/main/kotlin/com/lapots/gradle/plugins/appenv/core/exception/IOPluginException.kt
+++
@@ -1,7 +0,0 @@
-package com.lapots.gradle.plugins.appenv.core.exception
-
-/**
- * Exception for IO error handling in plugin.
- */
-class IOPluginException(val msg: String) : RuntimeException(msg) {
-}
